Used wrong values

plot.py
@@ -32,7 +32,7 @@ def array_from(data):
 	for d in data:
 		name, x, y, z, p = d
 		ax, ay, az = np.array(x), np.array(y), np.array(z)
-		q1, q2 = np.average(ax/ay), np.average(az/ay)
+		q1, q2 = np.average(ax/az), np.average(ay/az)
 		count = np.count_nonzero(np.abs(ay - az) > 1)
 		n = len(x)
 		l.append("\t{} & {} & {:.3f} & {:.3f} & {}/{}\\\\".format(name, p, q1, q2, count, n))
@@ -51,20 +51,26 @@ def boxplot(data):
 			lxy[name] = []
 			lzy[name] = []
 			n[name] = []
-		lxy[name].append(ax/ay) 
-		lzy[name].append(az/ay) 
+		lxy[name].append(ax/az) 
+		lzy[name].append(ay/az)
 		n[name].append(p)
+		# print(n[name])
+	# print(lxy, lzy, n)
 	for name in lxy:
+		print(n[name])
 		plt.figure()
 		plt.title("Ratio maxcut/p-maxcut for " + name)
-		plt.boxplot(lxy[name], labels=n[name],showfliers=False)
-		tpl.save("boxplot_xy_" + name +".tex")
+		plt.boxplot(lxy[name], labels=n[name],showfliers=True)
+		# plt.show()
+		tpl.save("boxplot_xy_" + name +"_outliers.tex")
 		
 		plt.figure()
-		plt.title("Ratio p-maxcut*/p-maxcut, p = " + name)
-		plt.boxplot(lzy[name], labels=n[name],showfliers=False)
+		plt.title("Ratio p-maxcut*/p-maxcut for  " + name)
+		plt.boxplot(lzy[name], labels=n[name],showfliers=True)
+		# print(lzy[name])
+		# plt.show()
 		# plt.savefig("boxplot_zy"+p+".pdf")
-		tpl.save("boxplot_zy_"+name+".tex")
+		tpl.save("boxplot_zy_"+name+"_outliers.tex")
 
 def array():
 	data = read_data(sys.argv[1])